cdf553f6488b7f43628423da57f80d0cb85cb720
SmGate <smartgatepk@gmail.com>
2024-09-21 10:55:50 +0500

    21-sep-2024

--- lib/Module/Login/Model/User.dart
+++ lib/Module/Login/Model/User.dart
@@ -19,24 +19,75 @@ class User {
   String? logo;
   String? societyName;
 
-  User(
-      {this.userId,
-      this.subadminid,
-      this.firstName,
-      this.lastName,
-      this.familyMemberId,
-      this.residentid,
-      this.cnic,
-      this.roleId,
-      this.roleName,
-      this.mobile,
-      this.bearerToken,
-      this.address,
-      this.societyId,
-      this.email,
-      this.hasCustomIntro,
-      this.image,
-      this.slogan,
-      this.societyName,
-      this.logo});
+  User({
+    this.userId,
+    this.subadminid,
+    this.firstName,
+    this.lastName,
+    this.familyMemberId,
+    this.residentid,
+    this.cnic,
+    this.roleId,
+    this.roleName,
+    this.mobile,
+    this.bearerToken,
+    this.address,
+    this.societyId,
+    this.email,
+    this.hasCustomIntro,
+    this.image,
+    this.slogan,
+    this.societyName,
+    this.logo,
+  });
+
+  // Convert User object to a Map
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'firstName': firstName,
+      'lastName': lastName,
+      'cnic': cnic,
+      'address': address,
+      'roleId': roleId,
+      'subadminid': subadminid,
+      'familyMemberId': familyMemberId,
+      'residentid': residentid,
+      'roleName': roleName,
+      'bearerToken': bearerToken,
+      'mobile': mobile,
+      'image': image,
+      'societyId': societyId,
+      'hasCustomIntro': hasCustomIntro,
+      'email': email,
+      'slogan': slogan,
+      'logo': logo,
+      'societyName': societyName,
+    };
+  }
+
+  // Create User from Map
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      userId: json['userId'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      cnic: json['cnic'],
+      address: json['address'],
+      roleId: json['roleId'],
+      subadminid: json['subadminid'],
+      familyMemberId: json['familyMemberId'],
+      residentid: json['residentid'],
+      roleName: json['roleName'],
+      bearerToken: json['bearerToken'],
+      mobile: json['mobile'],
+      image: json['image'],
+      societyId: json['societyId'],
+      hasCustomIntro: json['hasCustomIntro'],
+      email: json['email'],
+      slogan: json['slogan'],
+      logo: json['logo'],
+      societyName: json['societyName'],
+    );
+  }
 }
